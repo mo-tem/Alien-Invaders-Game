5e1d196e33e8fde30cf2787e7c68e00b320ca39f
mtemkit <temkit.m@gmail.com>
2023-01-21 08:23:05 -0500

    basic game created
    basic functionality completed as well the theme, sounds, images etc. focus was on using vanilla.js for the functionality and styling.

---
+++ README.md
@@ -0,0 +1,58 @@
+# Alien-Invaders-Game
+
+### Task:
+
+Making a fully functional space-invader game called "Alien Invaders".
+
+### Purpose:
+
+Improving my software development, design, and game development skills while building a fun game in the process.
+
+Also adding additional functionalities and refactoring my code for simplicity.
+
+### Languages:
+
+HTML, JavaScript (styling done entirely using JavaScript)
+
+### Hosting:
+
+Github Pages
+
+### How To Play:
+
+Move your mouse to move your player and align yourself with an enemy. Then, shoot lasers at the enemy with your space bar to score points.
+
+### Live Demo:
+
+https://mtemkit.github.io/
+
+Early stages of development:
+
+<p align="left">
+  <img src="images/game-start-screen.png" width="400" height="300" title="Alien Invaders Start Screen"></br>
+  <img src="images/gameplay.png" width="800" height="600" title="Alien Invaders Gameplay">
+</p>
+
+### Next Steps / Future Changes:
+
+- Add a pause and resume functionality.
+
+- Add a "play again" functionality.
+
+- Better color and design for the player, enemy, and background.
+
+- More effects to add during collisions.
+
+- Sound off/on buttons throughout gameplay.
+
+- Game Over sound effect and signal once an alien touches the floor.
+
+- Game Over Screen.
+
+- Celebration for each x10 milestone (level up sound effect, and randomly chosen congratularly image).
+
+- Game mp3 music made loopable beyond its length.
+
+- Music autoplay when it's first loaded.
+
+- Refactoring to ES6.
---
+++ game.js
@@ -0,0 +1,199 @@
+//----------------------------------- START GAME SCREEN----------------------------------
+
+var startGame = false;
+
+// Start Screen Background
+document.body.style.backgroundImage = "url('./images/space.gif')";
+document.body.style.backgroundSize = 'cover';
+
+// Create a title element
+var startTitle = document.createElement("div");
+startTitle.innerHTML = "Alien Invaders";
+startTitle.setAttribute("id", "startTitle");
+
+// Add the title to the body
+document.body.appendChild(startTitle);
+
+// Set the title style
+startTitle.style.textAlign = "center";
+startTitle.style.fontSize = "5em";
+startTitle.style.fontWeight = "bold";
+startTitle.style.color = "white";
+startTitle.style.position = "absolute";
+startTitle.style.top = "25%";
+startTitle.style.left = "50%";
+startTitle.style.transform = "translate(-50%, -50%)";
+startTitle.style.textShadow = "0px 0px 10px red";
+startTitle.style.marginBottom = "20px";
+
+// Create start button
+var startBtn = document.createElement('button');
+startBtn.innerHTML = 'Start Game';
+startBtn.setAttribute("id", "start-btn");
+
+// Set button styles
+startBtn.style.position = 'absolute';
+startBtn.style.top = '50%';
+startBtn.style.left = '50%';
+startBtn.style.transform = 'translate(-50%, -50%)';
+startBtn.style.backgroundColor = 'red';
+startBtn.style.color = 'white';
+startBtn.style.padding = '30px 60px';
+startBtn.style.borderRadius = '40px';
+startBtn.style.fontSize = '40px';
+startBtn.style.textAlign = 'center';
+startBtn.style.textDecoration = 'none';
+startBtn.style.boxShadow = '0px 0px 20px red';
+startBtn.style.transition = 'all 0.2s ease';
+
+
+// Add event listener for hover effect
+startBtn.addEventListener('mouseover', function() {
+    startBtn.style.boxShadow = '0px 0px 20px white';
+});
+
+// Add event listener to return button to normal state
+startBtn.addEventListener('mouseout', function() {
+    startBtn.style.boxShadow = '0px 0px 10px red';
+});
+
+
+// Add start button to the body
+document.body.appendChild(startBtn);
+
+
+startBtn.addEventListener('click', function() {
+    
+    // Start game code here
+    startBtn.style.display = 'none';
+    startTitle.style.display = "none";
+
+    // Set body background color to black
+    var backgroundMusic = new Audio('./sounds/music.mp3');
+    backgroundMusic.play();
+    
+    //-----------------------------------------GAME----------------------------------
+    //game background image
+    var background = document.createElement('img');
+    background.src = './images/space.gif';
+    background.style.position = 'absolute';
+    background.style.top = '0';
+    background.style.left = '0';
+    background.style.width = '100%';
+    background.style.height = '100%';
+    background.style.zIndex = '-1';
+    document.body.appendChild(background);
+    
+    // player image
+    var player = document.createElement('img');
+    player.src = './images/player-img.png';
+    player.style.position = 'fixed';
+    player.style.top = '50%';
+    player.style.left = '50%';
+    player.style.transform = 'translate(-50%, -50%)'
+    player.style.zIndex = '1';
+    player.style.width = '8%';
+    player.style.top = '95%'
+    document.body.appendChild(player);
+    player.style.backgroundColor = 'none';
+    player.style.filter = "drop-shadow(0 0 5px white)";
+    
+    //moving player left and right
+    document.addEventListener('mousemove', function (event) {
+        var left = event.clientX - player.offsetWidth / 2;
+        var right = event.clientX + player.offsetWidth / 2;
+        if (left < 0) {
+            player.style.left = player.offsetWidth / 2 + 'px';;
+        } else if (right > window.innerWidth) {
+            player.style.left = background.offsetWidth - player.offsetWidth / 2 + 'px';
+        } else {
+            player.style.left = left + 'px';
+        }
+    });
+    
+    document.addEventListener('keydown', function (event) {
+        if (event.keyCode === 32) {
+            //laser firing with space bar
+            var laser = document.createElement('img');
+            laser.src = './images/lasers-img.png';
+            laser.style.position = 'fixed';
+            laser.style.left = player.offsetLeft + player.offsetWidth / 2 - laser.offsetWidth / 2 - player.width / 2 + 'px';
+            laser.style.top = player.offsetTop + 'px';
+            document.body.appendChild(laser);
+            player.style.opacity = "0.8";
+            player.style.filter = "drop-shadow(0 0 5px red)";
+            var firing = new Audio('./sounds/laser.mp3');
+            firing.play();
+            var laserInterval = setInterval(function () {
+                laser.style.top = laser.offsetTop - 10 + 'px';
+                if (laser.offsetTop < 0) {
+                    //player.style.background = "none";
+                    player.style.opacity = '1';
+                    player.style.filter = "drop-shadow(0 0 5px white)";
+                    clearInterval(laserInterval);
+                    document.body.removeChild(laser);
+                }
+            }, 10);
+        }
+    });
+    
+    var enemyInterval = setInterval(function () {
+        var enemy = document.createElement('img');
+        enemy.src = './images/enemy-img.png';
+        enemy.style.position = 'fixed ';
+        enemy.style.width = '14%'
+        enemy.style.left = (Math.random() * (window.innerWidth - enemy.offsetWidth)) + 'px ';
+        enemy.style.top = '0 ';
+        enemy.style.filter = "drop-shadow(0 0 5px white)";
+        document.body.appendChild(enemy);
+        var enemyInterval = setInterval(function () {
+            enemy.style.top = enemy.offsetTop + 1 + 'px';
+            if (enemy.offsetTop > window.innerHeight) {
+                clearInterval(enemyInterval);
+                document.body.removeChild(enemy);
+            }
+        }, 10);
+    }, 1000);
+    
+    //scoring
+    var score = 0;
+    var scoreElement = document.createElement('div');
+    scoreElement.style.position = 'absolute';
+    scoreElement.style.top = '0';
+    scoreElement.style.left = '0';
+    scoreElement.style.color = 'white';
+    scoreElement.style.fontSize = '40px';
+    scoreElement.innerHTML = 'Score: ' + 0;
+    document.body.appendChild(scoreElement);
+    var laserInterval = setInterval(function () {
+        var lasers = document.querySelectorAll('img[src="./images/lasers-img.png"]');
+        var enemies = document.querySelectorAll('img[src="./images/enemy-img.png"]');
+        for (var i = 0; i < lasers.length; i++) {
+            for (var j = 0; j < enemies.length; j++) {
+                if (isCollision(lasers[i], enemies[j])) {
+                    document.body.removeChild(lasers[i]);
+                    document.body.removeChild(enemies[j]);
+                    var destroy = new Audio('./sounds/destroy.mp3');
+                    destroy.play();
+                    player.style.opacity = '1';
+                    player.style.filter = "drop-shadow(0 0 5px white)";
+                    score++;
+                    scoreElement.innerHTML = 'Score: ' + score;
+                }
+            }
+        }
+    }, 10);
+    
+    //collisions
+    function isCollision(a, b) {
+        var aRect = a.getBoundingClientRect();
+        var bRect = b.getBoundingClientRect();
+        return !(
+            aRect.top + aRect.height < bRect.top ||
+            aRect.top > bRect.top + bRect.height ||
+            aRect.left + aRect.width < bRect.left ||
+            aRect.left > bRect.left + bRect.width
+        );
+    }
+
+});
